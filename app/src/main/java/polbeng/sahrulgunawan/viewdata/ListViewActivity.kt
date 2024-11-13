9500e97fe428a38fc09d1a36e8cfd19ec481bb14
LENOVO <sahrulcc07@gmail.com>
2024-11-13 12:41:20 +0700

    Commit View Data

---
+++ app/src/main/java/polbeng/sahrulgunawan/viewdata/ListViewActivity.kt
@@ -0,0 +1,25 @@
+package polbeng.sahrulgunawan.viewdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import polbeng.sahrulgunawan.viewdata.databinding.ActivityListViewBinding
+import java.util.Arrays
+
+class ListViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var dataArray: Array<String> =
+            resources.getStringArray(R.array.jurusan)
+        Arrays.sort(dataArray)
+        var adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, dataArray)
+        binding.listView.adapter = adapter
+    }
+}
